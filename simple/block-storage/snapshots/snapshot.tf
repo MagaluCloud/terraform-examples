2004b9ca047d2ca843794bab6a32bed477ce29b9
lfpicoloto1 <lfpicoloto@gmail.com>
2024-07-12 10:41:40 -0300

    update block storage example

---
+++ simple/block-storage/snapshots/snapshot.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    mgc = {
+      source = "magalucloud/mgc"
+    }
+  }
+}
+
+data "terraform_remote_state" "volume" {
+  backend = "local" # ou outro backend que você esteja usando, como S3, etc.
+  config = {
+    path = "../volumes/terraform.tfstate" # caminho para o arquivo tfstate gerado pelo arquivo volume.tf
+  }
+}
+
+resource "mgc_block_storage_snapshots" "snapshot_example" {
+  description = "exemplo de snapshot descrição"
+  name = "exemplo snapshot name"
+  volume = {
+    id = data.terraform_remote_state.volume.outputs.example_volume_id
+  }
+}
