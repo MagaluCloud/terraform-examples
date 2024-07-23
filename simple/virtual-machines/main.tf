caa3ee16ed94d32d2b77d7ddc1b6fbdee56ea8c9
lfpicoloto1 <lfpicoloto@gmail.com>
2024-07-23 10:28:29 -0300

    update examples in vm

--- simple/virtual-machines/main.tf
+++ simple/virtual-machines/main.tf
@@ -1,14 +1,14 @@
 terraform {
   required_providers {
     mgc = {
-      source = "magalucloud/mgc"
+      source = "local/magalu/mgc"
     }
   }
 }
 
 
 # Create a VM at Nordeste br-ne1
-resource "mgc_virtual-machine_instances" "basic_instance_nordeste" {
+resource "mgc_virtual_machine_instances" "basic_instance_nordeste" {
   provider = mgc.nordeste # We specify the provider region here to indicate that this VM should be created in the Nordeste region.
   name = "basic-instance-nordeste"
   machine_type = {
@@ -19,15 +19,16 @@ resource "mgc_virtual-machine_instances" "basic_instance_nordeste" {
   }
   network = {
     associate_public_ip = false # If true, will create a public IP
+    delete_public_ip = false
   }
-  delete_public_ip = false
+  
   ssh_key_name   = "ssh_key"
 }
 
 # Create a VM at Sudeste br-se1
-resource "mgc_virtual-machine_instances" "basic_instance" {
+resource "mgc_virtual_machine_instances" "basic_instance_sudeste" {
   provider = mgc.sudeste
-  name = "basic-instance"
+  name = "basic-instance-sudeste"
   machine_type = {
     name = "cloud-bs1.xsmall"
   }
@@ -36,15 +37,16 @@ resource "mgc_virtual-machine_instances" "basic_instance" {
   }
   network = {
     associate_public_ip = false
+    delete_public_ip = false
   }
-  delete_public_ip = false
+  
   ssh_key_name   = "ssh_key"
 }
 
 # Create a VM and associate a Public IP
-resource "mgc_virtual-machine_instances" "basic_instance" {
+resource "mgc_virtual_machine_instances" "basic_instance_public_ip" {
   provider = mgc.sudeste
-  name = "basic-instance"
+  name = "basic-instance-public-ip"
   machine_type = {
     name = "cloud-bs1.xsmall"
   }
@@ -53,14 +55,15 @@ resource "mgc_virtual-machine_instances" "basic_instance" {
   }
   network = {
     associate_public_ip = true
+    delete_public_ip = true # We specify that when this VM is deleted, the public IP should be deleted as well.
   }
-  delete_public_ip = true # We specify that when this VM is deleted, the public IP should be deleted as well.
+  
   ssh_key_name   = "ssh_key"
 }
 
 # Create a VM and associate with specific Security Group
-resource "mgc_virtual-machine_instances" "basic_instance_with_SG" {
-  name = "basic-instance-nordeste"
+resource "mgc_virtual_machine_instances" "basic_instance_with_SG" {
+  name = "basic-instance-nordeste-with-sg"
   machine_type = {
     name = "cloud-bs1.xsmall"
   }
@@ -70,11 +73,12 @@ resource "mgc_virtual-machine_instances" "basic_instance_with_SG" {
   network = {
     associate_public_ip = false # If true, will create a public IP
     interface = {
-      security_groups = [
-        {id = "aa622bcb-6861-4251-9cdb-aaadf3" }
-      ]
+      security_groups = {
+          id = ["aa622bcb-6861-4251-9cdb-aaadf3" ]
+        }
+      
     }
+    delete_public_ip = false
   }
-  delete_public_ip = false
   ssh_key_name   = "ssh_key"
 }
