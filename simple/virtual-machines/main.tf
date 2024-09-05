02dab89c516cfbb9d9acdf1cc57930961ef6b8ae
Lucas Picoloto <lfpicoloto@gmail.com>
2024-09-05 10:56:57 -0300

    add example with provisioner

--- simple/virtual-machines/main.tf
+++ simple/virtual-machines/main.tf
@@ -61,9 +61,9 @@ resource "mgc_virtual_machine_instances" "basic_instance_public_ip" {
   ssh_key_name = "ssh_key"
 }
 
-# Create a VM and associate with specific Security Group
-resource "mgc_virtual_machine_instances" "basic_instance_with_SG" {
-  name = "basic-instance-nordeste-with-sg"
+# Create a vm and execute provisioner
+resource "mgc_virtual_machine_instances" "basic_instance_using-provisioner" {
+  name = "basic_instance_using-provisioner"
   machine_type = {
     name = "cloud-bs1.xsmall"
   }
@@ -71,11 +71,30 @@ resource "mgc_virtual_machine_instances" "basic_instance_with_SG" {
     name = "cloud-ubuntu-22.04 LTS"
   }
   network = {
-    associate_public_ip = false # If true, will create a public IP
-    delete_public_ip = false
+    associate_public_ip = true # If true, will create a public IP
+    delete_public_ip = true
     interface = {
-      security_groups = [{ "id" : "2b4d2ec6-5871-4f4a-8e5e-d13c154b8c61" }]
+      security_groups = [{ "id" : "rw2r4123-e961-4916-b282-113123123" }]
     }
   }
-  ssh_key_name = "ssh_key"
+  
+  ssh_key_name = "ssh-key"
+  
+  provisioner "file" {
+    source      = "hello.txt"
+    destination = "/tmp/hello.txt"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/hello.txt",
+      "ls -l /tmp/hello.txt"
+    ]
+  }
+  connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file("~/.ssh/ssh-key")
+      host        = self.network.public_address
+    }
 }
+
