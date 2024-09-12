006703280c0f63d28b4b25ed9410287fd9592c87
lfpicoloto1 <133693873+lfpicoloto1@users.noreply.github.com>
2024-09-12 18:01:04 -0300

    Update main.tf

--- advanced/kubernetes-with-kubeconfig/main.tf
+++ advanced/kubernetes-with-kubeconfig/main.tf
@@ -1,27 +1,15 @@
 # Criando um cluster com um nodepool
-resource "mgc_kubernetes_cluster" "cluster_with_nodepool" {
+resource "mgc_kubernetes_cluster" "cluster" {
   name                 = var.cluster_name
-  enabled_bastion      = false
   version              = var.kubernetes_version
   enabled_server_group = false
   description          = var.cluster_description
-  node_pools = [{
-    name     = var.nodepool_name
-    replicas = var.nodepool_replicas
-    flavor   = var.nodepool_flavor
-  }]
 }
 
-# Tempo de espera para o cluster ficar ativo
-# Ajuste o tempo conforme necessário
-resource "time_sleep" "wait_15_minutes" {
-  depends_on      = [mgc_kubernetes_cluster.cluster_with_nodepool]
-  create_duration = var.timer_duration
-}
 
 # Criando um nodepool
 resource "mgc_kubernetes_nodepool" "gp1_small" {
-  depends_on = [time_sleep.wait_15_minutes] # Wait timer
+  depends_on = [mgc_kubernetes_cluster.cluster_with_nodepool]
   name       = "apis-2cpu-4gb-20gb"
   cluster_id = mgc_kubernetes_cluster.cluster_with_nodepool.id
   flavor     = var.nodepool_flavor
@@ -32,11 +20,6 @@ resource "mgc_kubernetes_nodepool" "gp1_small" {
   }
 }
 
-# Timer para esperar o cluster ficar ativo
-resource "time_sleep" "wait_for_cluster" {
-  depends_on      = [mgc_kubernetes_cluster.cluster_with_nodepool]
-  create_duration = "10m" # Ajuste o tempo conforme necessário
-}
 
 # Pegar o kubeconfig do cluster usando o output do cluster_id
 data "mgc_kubernetes_cluster_kubeconfig" "cluster" {
